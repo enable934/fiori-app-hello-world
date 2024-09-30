d62142d5e63923f99c587b6735407d7efe1b5a0c
Orest Novak <black.novak.orest@gmail.com>
2024-09-30 16:46:58 +0000

    implement basic counter

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,14 +1,36 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller, MessageBox) {
+function (Controller, MessageBox, JSONModel) {
     "use strict";
 
     return Controller.extend("sap.btp.helloworldui5.controller.View1", {
         onInit: function () {
-
+            let oModel = new JSONModel({
+                counter: 0
+            });
+            this.getView().setModel(oModel);
         },
-        onPress: function () { MessageBox.alert("Heelloss")}
+        onFirstBtnPress: function () { 
+            const oModel = this.getView().getModel();
+            const iCounter = 0;
+            oModel.setProperty("/counter", iCounter);
+            MessageBox.success(`Counter successfully cleared`);
+        },
+        onSecondBtnPress: function () { 
+            const oModel = this.getView().getModel();
+            let iCounter = oModel.getProperty("/counter");
+            iCounter++;
+            oModel.setProperty("/counter", iCounter);
+            MessageBox.information(`You clicked on btn ${iCounter} times`);
+        },
+        onThirdBtnPress: function () { 
+            const oModel = this.getView().getModel();
+            let iCounter = oModel.getProperty("/counter");
+            iCounter += 1000;
+            oModel.setProperty("/counter", iCounter);
+        }
     });
 });
